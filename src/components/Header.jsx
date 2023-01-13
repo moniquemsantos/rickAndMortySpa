05d777484d47f342476003a101bb0aa071545aa8
Monique Santos <monique@blackmagicbox.com.br>
2023-01-13 04:02:09 +0100

    Feat Search bar (filter characters) (#4)
    Co-authored-by: Monique Santos <moniquemattosm@gmail.com>

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,71 @@
+import {alpha, AppBar, InputBase, styled, Toolbar, Typography} from "@mui/material";
+import SearchIcon from '@mui/icons-material/Search';
 import './header.css';
 
-function Header() {
-  return <h1 className="heading">Rick and Morty!</h1>;
+const Search = styled('div')(({theme}) => ({
+  position: 'relative',
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  '&:hover': {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginRight: theme.spacing(2),
+  marginLeft: 0,
+  width: '100%',
+  [theme.breakpoints.up('sm')]: {
+    marginLeft: theme.spacing(3),
+    width: 'auto',
+  },
+}));
+
+const SearchIconWrapper = styled('div')(({theme}) => ({
+  padding: theme.spacing(0, 2),
+  height: '100%',
+  position: 'absolute',
+  pointerEvents: 'none',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+}));
+
+const StyledInputBase = styled(InputBase)(({theme}) => ({
+  color: 'inherit',
+  '& .MuiInputBase-input': {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('md')]: {
+      width: '20ch',
+    },
+  },
+}));
+
+
+function Header({handleSearch}) {
+  return (
+    <header className="heading">
+      <AppBar position="fixed">
+        <Toolbar>
+          <Typography variant="h1" color="inherit" component="div" fontSize={30}>
+            Rick and Morty!
+          </Typography>
+          <Search>
+            <SearchIconWrapper>
+              <SearchIcon/>
+            </SearchIconWrapper>
+            <StyledInputBase
+              placeholder="Search…"
+              inputProps={{'aria-label': 'search'}}
+              onChange={(event) => handleSearch(event.target.value)}
+            />
+          </Search>
+        </Toolbar>
+      </AppBar>
+      <Toolbar/>
+    </header>
+  )
 }
 
 export default Header;
