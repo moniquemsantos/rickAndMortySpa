0d7233e621e2f77222c75031a0072345a51abd1b
Monique Santos <moniquemattosm@gmail.com>
2023-01-13 02:37:21 +0100

    feat modal dialog [draft]

--- src/components/CharacterCard.jsx
+++ src/components/CharacterCard.jsx
@@ -1,9 +1,16 @@
 import React from "react";
-import "./CharacterCard.css"
+import Box from '@mui/material/Box';
 import {Button} from "@mui/material";
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
+import "./CharacterCard.css"
 
 
 function CharacterCard({character}) {
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
   return (
     <div className="flip-card">
       < div className="flip-card-inner">
@@ -12,12 +19,27 @@ function CharacterCard({character}) {
         </div>
         <div className="flip-card-back">
           <p>{character.name}</p>
-          <Button variant="contained" color="primary">More Info</Button>
-        </div>
+          <Button onClick={handleOpen} variant="contained">Read More</Button>
+          <Modal
+            open={open}
+            onClose={handleClose}
+            aria-labelledby="modal-modal-title"
+            aria-describedby="modal-modal-description"
+          >
+            <Box>
+              <Typography id="modal-modal-title" variant="h6" component="h2">
+                Text in a modal
+              </Typography>
+              <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+              </Typography>
+            </Box>
+          </Modal>
+          
+          
+        </div>  
       </div>
     </div>
-
-
   );
 }
 
